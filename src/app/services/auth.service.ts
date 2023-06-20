2ae56a714ec9b7200604a502a8359e789ba3783c
Clinton-Rodrigues <clintonrodrigues.dev@gmail.com>
2023-06-20 18:57:32 -0300

    Build

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,13 +4,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
+import { environment } from './config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3333/api';
-  private tokenCookieName = 'authToken'; // Nome do cookie que armazena o token
+  private apiUrl = environment.apiUrl;
+
+  private tokenCookieName = environment.tokenCookieName;
+ // Nome do cookie que armazena o token
 
   constructor(private http: HttpClient, private cookieService: CookieService) {}
 
---
+++ src/app/services/config.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    apiUrl: 'https://polar-stream-75434-ae90664990a9.herokuapp.com/api',
+    tokenCookieName: 'authToken'
+  };
+  
